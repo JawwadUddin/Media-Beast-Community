ce6bee956252b4c298df5db60b879c9ec9844603
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-08 23:31:08 +0000

    Add cors to express app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.sqlite");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
