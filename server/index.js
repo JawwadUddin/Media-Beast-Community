cd24b1cffc9a9df69eed64f85a86d5af721e927a
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-08 23:14:19 +0000

    Route - obtain room data from db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database.sqlite");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the media beast community server");
+});
+
+app.get("/rooms", (req, res) => {
+  db.all(`SELECT * FROM rooms`, [], (error, rows) => {
+    if (error) {
+      console.error(`Error querying data from rooms table:`, error.message);
+      res
+        .status(500)
+        .send(`Error querying data from rooms table: ${error.message}`);
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
