c1c1ff2751cbe29dd28c313ba1e5ac9d5d19292b
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-08 20:36:29 +0000

    Install react-icons, improve sidebar

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -1,4 +1,5 @@
 import dummyData from "../data/dummyData.json";
+import { FaLock } from "react-icons/fa";
 
 type Rooms = {
   id: number;
@@ -16,7 +17,10 @@ const Sidebar = () => {
       <ul className="sidebar-items">
         <li className="selected">All</li>
         {rooms.map((room) => (
-          <li>{room.name}</li>
+          <li>
+            <span>{room.name}</span>
+            <FaLock size={12} />
+          </li>
         ))}
       </ul>
     </div>
