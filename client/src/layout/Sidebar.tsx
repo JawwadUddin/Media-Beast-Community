34fa7f4c7b044cc817a949a6a3acb9568dd1b034
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-09 21:07:19 +0000

    Replace dummy room data with server data

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -1,22 +1,23 @@
 import { useContext } from "react";
-import dummyData from "../data/dummyData.json";
 import { FaLock } from "react-icons/fa";
 import { IoMdCheckmark } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 import UserContext from "../context/userContext";
 import DataContext from "../context/dataContext";
-
-type Rooms = {
-  id: number;
-  name: string;
-  description: string;
-  img: string;
-};
+import useRooms from "../hooks/useRooms";
 
 const Sidebar = () => {
-  const rooms: Rooms[] = dummyData;
   const { userInfo } = useContext(UserContext);
   const { applications } = useContext(DataContext);
+  const { loading, error, data: rooms } = useRooms();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div className="sidebar-container">
@@ -36,7 +37,7 @@ const Sidebar = () => {
           <h2>Requests</h2>
           <ul className="sidebar-requests">
             {applications?.map((application) => (
-              <li>
+              <li key={application.id}>
                 <h3>{application.email}</h3>
               </li>
             ))}
