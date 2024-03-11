e537dfc7ec2b0ca5ec8a1867c94be8a287234185
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-11 01:42:43 +0000

    Allow users with accepted status to bypass protected route

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,16 +1,29 @@
 import { ReactNode, useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import UserContext from "../context/userContext";
+import DataContext from "../context/dataContext";
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { userInfo } = useContext(UserContext);
+  const { userApplications } = useContext(DataContext);
+
+  const { roomId } = useParams();
+
+  const isAllowed =
+    roomId &&
+    userApplications?.find(
+      (application) => application.roomId === parseInt(roomId)
+    )?.status === "accepted";
+
+  console.log(isAllowed);
 
   const isAuthorized = userInfo && userInfo.role === "moderator";
 
-  if (!isAuthorized) {
+  if (isAuthorized || isAllowed) {
+    return children;
+  } else {
     return <Navigate to="/unauthorised" replace />;
   }
-  return children;
 };
 
 export default ProtectedRoute;
