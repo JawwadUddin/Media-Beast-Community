c2773601a78c0a73cdaea042c67106680af0574a
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-11 01:40:11 +0000

    Add useUserApplications to context

--- client/src/context/dataContext.tsx
+++ client/src/context/dataContext.tsx
@@ -1,8 +1,10 @@
 import { ReactNode, createContext, useState } from "react";
 import useApplications, { Applications } from "../hooks/useApplications";
+import useUserApplications from "../hooks/useUserApplications";
 
 type DataContext = {
   applications: Applications[] | null;
+  userApplications: Applications[] | null;
   selectedRoom: number | null;
   selectRoom: (roomId: number | null) => void;
   setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,13 +14,20 @@ const DataContext = createContext({} as DataContext);
 
 export const DataProvider = ({ children }: { children: ReactNode }) => {
   const { data: applications, setRefresh } = useApplications();
+  const { data: userApplications } = useUserApplications();
   const [selectedRoom, setSelectedRoom] = useState<number | null>(null);
   function selectRoom(roomId: number | null) {
     setSelectedRoom(roomId);
   }
   return (
     <DataContext.Provider
-      value={{ applications, selectedRoom, selectRoom, setRefresh }}
+      value={{
+        applications,
+        userApplications,
+        selectedRoom,
+        selectRoom,
+        setRefresh,
+      }}
     >
       {children}
     </DataContext.Provider>
