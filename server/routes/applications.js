51d13113b5f20a83058f8e5010b3b91508a90512
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-11 00:11:21 +0000

    Route to update application status

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -20,4 +20,47 @@ router.get("/", (req, res) => {
   );
 });
 
+router.put("/:applicationId/update-application", (req, res) => {
+  const applicationId = req.params.applicationId;
+  const applicationStatus = req.body.applicationStatus;
+
+  // Query to get the statusId based on the applicationStatus
+  db.get(
+    "SELECT id FROM application_status WHERE status = ?",
+    [applicationStatus],
+    (err, row) => {
+      if (err) {
+        console.error("Error updating application:", err.message);
+        res.status(500).send("Error updating application status");
+        return;
+      }
+
+      if (!row) {
+        console.error("Invalid application status:", applicationStatus);
+        res.status(400).send("Invalid application status");
+        return;
+      }
+
+      const statusId = row.id;
+
+      // Update the statusId for the application with the given applicationId
+      db.run(
+        "UPDATE applications SET statusId = ? WHERE id = ?",
+        [statusId, applicationId],
+        function (err) {
+          if (err) {
+            console.error("Error updating application:", err.message);
+            res.status(500).send("Error updating application status");
+            return;
+          }
+          console.log(`Application ${applicationId} updated successfully`);
+          res
+            .status(200)
+            .send(`Application ${applicationId} updated successfully`);
+        }
+      );
+    }
+  );
+});
+
 module.exports = router;
