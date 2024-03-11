f9564147e2b42498da6689ae36ad1fd266624a11
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-03-11 01:39:46 +0000

    Custom hook to get individual user applications

---
+++ client/src/hooks/useUserApplications.ts
@@ -0,0 +1,61 @@
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import UserContext from "../context/userContext";
+
+export type Applications = {
+  id: number;
+  userId: number;
+  roomId: number;
+  statusId: number;
+  createdAt: Date;
+  status: string;
+  email: string;
+};
+
+const useUserApplications = () => {
+  const [loading, setLoading] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+  const [error, setError] = useState<any>(null);
+  const [data, setData] = useState<Applications[]>([]);
+  const { userInfo } = useContext(UserContext);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const fetchApplications = async () => {
+      setRefresh(false);
+      try {
+        const response = await axios.get<Applications[]>(
+          `http://localhost:5000/api/applications/${userInfo.id}`,
+          {
+            cancelToken: source.token,
+          }
+        );
+        setData(response.data);
+      } catch (err) {
+        if (axios.isCancel(err)) {
+          console.log("Request canceled:", err.message);
+        } else {
+          setError(err);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (userInfo?.role === "user") {
+      fetchApplications();
+    } else {
+      setLoading(false);
+    }
+
+    // Cleanup function
+    return () => {
+      source.cancel("Component unmounted");
+    };
+  }, [userInfo?.role, refresh]);
+
+  return { loading, error, data, setRefresh };
+};
+
+export default useUserApplications;
